0ba9c83777578e12b7a783b6d41405aefa68ad96
o-holloway <156986161+o-holloway@users.noreply.github.com>
2024-03-07 10:28:30 -0500

    fixed sound

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import mixer
 from wrappers import PokeAPI
 import urllib.request 
+import time
 
 client = PokeAPI()
 
@@ -13,10 +14,12 @@ poke_info = client.get_poke_info(poke_name)
 if poke_info != None:
     print(poke_info)
     if input(f"Listen to {poke_name}'s default sound (Y/N)? ").lower() == "y":
-        urllib.request.urlretrieve(poke_info.cry, 'temp.ogg')
         mixer.init()
+        urllib.request.urlretrieve(poke_info.cry, 'temp.ogg')
         mixer.music.load('temp.ogg')
         mixer.music.set_volume(0.2)
         mixer.music.play()
+        time.sleep(5)
+        print("Pokemon sound played!")
 else:
     print("No information found")
